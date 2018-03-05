27a12b2dabac41031f3ed2b676fbb3a0b5006677
cameron <cameron.grams.2@gmail.com>
2018-03-04 22:34:54 -0500

    non-working first test

---
+++ src/setupTests.js
@@ -0,0 +1,9 @@
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({adapter: new Adapter()});
+
+
+
+//  https://medium.com/netscape/testing-a-react-redux-app-using-jest-and-enzyme-b349324803a9
+
