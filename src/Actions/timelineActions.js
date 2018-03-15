41a318daa8e4925ebe83d6afbe0173e16bf0ad74
Cameron <cameron.grams.2@gmail.com>
2018-03-15 08:56:09 -0400

    add in netlify

--- src/Actions/timelineActions.js
+++ src/Actions/timelineActions.js
@@ -1,7 +1,8 @@
 import * as actionTypes from './actionTypes'; 
 import { push } from 'react-router-redux';
 
-const endpoint = "http://localhost:3030"; 
+// const endpoint = "http://localhost:3030"; 
+const endpoint = "https://aqueous-fjord-19217.herokuapp.com/";
 
 export function loadCurrentEntry( entry ){
     return{
