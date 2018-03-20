c312ae9ad7f0ea7e84b52161127dfac1476ca9da
Cameron <cameron.grams.2@gmail.com>
2018-03-20 13:30:36 -0400

    working entry alignment, work on styles

--- src/Components/RenderField/renderField.jsx
+++ src/Components/RenderField/renderField.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
   const renderField = ( { className, type, label, labelClassName, input, inputClassName, meta: { touched, error } } ) => (
     <div className={ className } >
-      <div className  className={ `${ labelClassName}` } ><label >{ label }</label><span>  </span></div>
+      <div className={ `${ labelClassName}` } ><label >{ label }</label><span>  </span></div>
       <div className={ `${ inputClassName}` } > <input  { ...input } type={ type } /></div>
       { touched && error && <span className="error" >{ error }</span> }
     </div>
