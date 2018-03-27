d18752a043ac60e51cb4f5f561bb979b54cecdf1
Cameron <cameron.grams.2@gmail.com>
2018-03-27 08:44:25 -0400

    ts mongo

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -2,7 +2,6 @@ export const endpoint = "https://aqueous-fjord-19217.herokuapp.com";
 
 // export const endpoint = "http://localhost:3030";
 
-
 export const appConfig = {
     NOT_FOUND_ENDPOINT:  "/error-page",
     UNAUTHORIZED_ENDPOINT: "/error-page",
