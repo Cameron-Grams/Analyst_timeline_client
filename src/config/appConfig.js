706bcc2aa5446975aeccf431b23b912bd09ec331
cameron <cameron.grams.2@gmail.com>
2018-05-02 21:06:52 -0400

    walk through the flow in tests

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 export const endpoint =  "http://localhost:3030" || "https://aqueous-fjord-19217.herokuapp.com";
-=======
-export const endpoint =   "http://localhost:3030" || "https://aqueous-fjord-19217.herokuapp.com";
-
->>>>>>> 684bb5956bf1673fae3775a536d43aa7ebb3798f
 
 // export const endpoint = "http://localhost:3030";
 
